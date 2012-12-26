a8d83284edba37cdfd529df8241132f269d0b243
Jason Casey <unblockgames@gmail.com>
2012-12-25 21:04:36 -0600

    I cant figure out why it just stops working
    The first set of queries work it updates no problem. but the second set
    does not update for some reason. Its like the click even stops firing...

--- application/controllers/browser.php
+++ application/controllers/browser.php
@@ -10,23 +10,18 @@ class Browser extends CI_Controller {
 		$data['result'] = array(); //initialize the array that will hold the arrays of data
 		$data['result'] = $this->db->query('SELECT * FROM `invmarketgroups` WHERE `parentGroupID` IS NULL ORDER BY `invmarketgroups`.`marketGroupName` ASC'); // query the database to retrieve the initial base market catagories
 		$this->load->view('browserview', $data); // load the view and send the view the data we've created here
-		global $history;
 	}
 	public function getNext($q) //this function is called by jquery to update the database listings
 	{
-		/*
+		static $history = array();
 		array_push($history,$q); //keep track of the ids that the user is navigating through because we need them to go backward through the browser
 		$this->load->database(); // load the database class
-		$result = $this->db->query('SELECT * FROM `invmarketgroups` WHERE `parentGroupID` '.$q); //query the database based on $q which is the marketgroupid
-		while ($row = mysql_fetch_array($data['query'])) //for each market catagory extract the data and put it into the form of a php array
-		{
-			if $row['hasTypes'] = '1' //if the market group has items
-					$result = $this->db->query('SELECT * FROM `invmarketgroups` WHERE `parentGroupID` '.$q);//specialized query that requests items
-				else
-					//query that gets the next set of catagories
-			endif
-		}*/
-		echo "<li>".$q."</li>";
+		$result = $this->db->query("SELECT * FROM `invmarketgroups` WHERE `parentGroupID` = '".$q."' ORDER BY `invmarketgroups`.`marketGroupName` ASC"); //query the database based on $q which is the marketgroupid
+		
+		foreach ($result->result_array() as $row)
+			{
+				echo '<li q = "'.$row['marketGroupID'].'">'.$row['marketGroupName'].'</li>';
+			}
 	}
 	public function getLast($q) //this function is called by jquery to update the database listings
 	{
